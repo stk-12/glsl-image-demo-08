28542d420cc5869787f22e054823b7445d4446d5
keisuke sato <corydoras121@gmail.com>
2023-05-18 00:07:59 +0900

    first

---
+++ src/js/shader/vertexShader.glsl
@@ -0,0 +1,18 @@
+varying vec2 vUv;
+varying float vDistortion;
+uniform float uProg;
+uniform float uScroll;
+
+
+float PI = 3.1415926535897932384626433832795;
+
+void main() {
+  vUv = uv;
+  vec3 pos = position;
+
+  float scroll = uScroll * 0.005;
+
+  pos.y += cos(pos.x * PI) * 0.08 * scroll;
+
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+}
